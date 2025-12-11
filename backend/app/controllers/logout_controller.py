28c476d9b22a952857b52b6c14f0eb811855ed7c
pujyank2006 <pujyank2006@gmail.com>
2025-12-11 08:21:57 +0530

    .

--- backend/app/controllers/logout_controller.py
+++ backend/app/controllers/logout_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, make_response
-from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt, unset_jwt_cookies
 
 logout_bp = Blueprint("logout", __name__)
 
