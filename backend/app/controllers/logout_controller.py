d1fae8ad286ff1b7a8bb2fc967678485ae7d127e
pujyank2006 <pujyank2006@gmail.com>
2025-12-11 08:14:29 +0530

    .

--- backend/app/controllers/logout_controller.py
+++ backend/app/controllers/logout_controller.py
@@ -4,13 +4,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 logout_bp = Blueprint("logout", __name__)
 
 @logout_bp.post("/logout")
-@jwt_required()
 def logout():
-    current_user = get_jwt_identity()
-    if not current_user:
-        return jsnoify({
-            "message": "No user"
-        })
     response = make_response(jsonify({"message": "Logout successful"}))
     # Overwrite the cookie with an empty value and immediate expiry
     response.set_cookie("access_token_cookie", "", expires=0)
